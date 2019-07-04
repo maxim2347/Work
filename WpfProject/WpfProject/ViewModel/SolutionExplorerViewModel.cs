b4fa53f4f188ae0093cde45e925170fffe1e7751
maxsim2347 <maxsim2347@gmail.com>
2019-07-04 14:23:25 +0300

    treeview

--- WpfProject/WpfProject/ViewModel/SolutionExplorerViewModel.cs
+++ WpfProject/WpfProject/ViewModel/SolutionExplorerViewModel.cs
@@ -1,40 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using WpfProject.Common;
 using WpfProject.Model;
 
 namespace WpfProject.ViewModel {
     public class SolutionExplorerViewModel : BaseVM {
         public List<ProjectItem> Items { get; }
-        
+     
         ProjectItem selectedItem;
-        public ProjectItem SelectedItem { get { return selectedItem; } set { SetProperty(ref selectedItem, value); } } 
+        public ProjectItem SelectedItem { get { return selectedItem; } set { SetProperty(ref selectedItem, value); } }
+
+        void GetDirectoryTree(string start ,ProjectItem root) {
+            string[] dd = Directory.GetDirectories(start);
+            foreach(var x in dd) {
+                string x1 = x;
+                x1.Remove(0,start.Length);
+                ProjectItem item = new ProjectItem() { Name = x.Remove(0, start.Length+1), Type =ProjectItemType.Folder };
+                root.Items.Add(item);
+                GetDirectoryTree(x, item);
+            }
+            foreach(var y in Directory.GetFiles(start)) {
+                ProjectItem file = new ProjectItem() { Name = y.Remove(0, start.Length + 1), Type=ProjectItemType.File };
+                root.Items.Add(file);
+            }
+        }
 
         public SolutionExplorerViewModel() {
-            Items = new List<ProjectItem>();
-         
-            var projectA = new ProjectItem() {
-                Name = "ProjectA",
-                Type = ProjectItemType.Project,
-            };
-            var projectProperties = new ProjectItem() {
-                Name = "Properties",
-                Type = ProjectItemType.Folder,
-            };
-            var projectPropertiesFile = new ProjectItem() {
-                Name = "Settings.txt",
-                Type = ProjectItemType.File,
-                Text = "Settings.txt text text",
-            };
-            projectProperties.Items.Add(projectPropertiesFile);
-            projectA.Items.Add(projectProperties);
-
-            var file = new ProjectItem() {
-                Name = "File.txt",
-                Type = ProjectItemType.File,
-                Text = "File.txt text text",
-            };
-            projectA.Items.Add(file);
-            Items.Add(projectA);
+            string startDirectory = @"C:\Work\Work\WpfProject\ProjectA";
+            Items= new List<ProjectItem>();
+            ProjectItem Root = new ProjectItem() { Name = "ProjectA", Type = ProjectItemType.Project };
+            Items.Add(Root);
+           
+            GetDirectoryTree(startDirectory,Root);
+            
         }
     }
 }

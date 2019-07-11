d507d4f175956a13f3006fef7c55c34ed2e1842f
maxsim2347 <maxsim2347@gmail.com>
2019-07-11 12:46:47 +0300

    refactoring

--- WpfProject/WpfProject/MainWindow.xaml.cs
+++ WpfProject/WpfProject/MainWindow.xaml.cs
@@ -1,46 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using WpfProject.Model;
-using WpfProject.ViewModel;
+﻿using System.Windows;
 
 namespace WpfProject {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-        }
-      
-        void OnTreeViewPreviewMouseDoubleClick(object sender, MouseButtonEventArgs e) {
-            var dc = DataContext as SolutionExplorerViewModel;
-
-            TreeViewItem clickedItem = TryGetClickedItem(tree, e);
-            if(clickedItem == null)
-                return;
-
-            dc.SelectedItem = (ProjectItem)clickedItem.DataContext;
-            if(dc.SelectedItem?.Type == ProjectItemType.File) {
-                bool isAlradyTab = false;
-                foreach(var x in dc.Tabs) {
-                    if(x.Name == dc.SelectedItem.Name)
-                        isAlradyTab = true;
-                }
-                if(!isAlradyTab)dc.Tabs.Add(dc.SelectedItem);
-            }
-        }
-
-        TreeViewItem TryGetClickedItem(TreeView treeView, MouseButtonEventArgs e) {
-            var hit = e.OriginalSource as DependencyObject;
-            while(hit != null && !(hit is TreeViewItem))
-                hit = VisualTreeHelper.GetParent(hit);
-            return hit as TreeViewItem;
-        }
-
-        private void Tree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
-            var dc = DataContext as SolutionExplorerViewModel;
-            dc.SelectedItem = e.NewValue as ProjectItem;
         }
     }
 }
---
+++ WpfProject/WpfProject/View/MainView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using WpfProject.Model;
+using WpfProject.ViewModel;
+
+namespace WpfProject.View {
+    public partial class MainView : UserControl {
+        public MainView() {
+            InitializeComponent();
+        }
+        void OnTreeViewPreviewMouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            var dc = DataContext as SolutionExplorerViewModel;
+
+            TreeViewItem clickedItem = TryGetClickedItem(tree, e);
+            if(clickedItem == null)
+                return;
+
+            dc.SelectedItem = (ProjectItem)clickedItem.DataContext;
+            if(dc.SelectedItem?.Type == ProjectItemType.File) {
+                bool isAlradyTab = false;
+                foreach(var x in dc.Tabs) {
+                    if(x.Name == dc.SelectedItem.Name)
+                        isAlradyTab = true;
+                }
+                if(!isAlradyTab) dc.Tabs.Add(dc.SelectedItem);
+            }
+        }
+
+        TreeViewItem TryGetClickedItem(TreeView treeView, MouseButtonEventArgs e) {
+            var hit = e.OriginalSource as DependencyObject;
+            while(hit != null && !(hit is TreeViewItem))
+                hit = VisualTreeHelper.GetParent(hit);
+            return hit as TreeViewItem;
+        }
+
+        private void Tree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            var dc = DataContext as SolutionExplorerViewModel;
+            dc.SelectedItem = e.NewValue as ProjectItem;
+        }
+    }
+}
